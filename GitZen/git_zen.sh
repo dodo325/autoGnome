f8d51822fa5578ce3b628efdd09cce6f30708f0f
dodo325 <dodo101000101@gmail.com>
2018-12-08 17:52:59 +0300

    feat(Добавил инфу про commitizen): add commitizen
    Я взял и добавил инструкции по установке nodejs npm git с последующей
    установкой
    commitizen и cz-customizable

--- GitZen/git_zen.sh
+++ GitZen/git_zen.sh
@@ -1,4 +1,16 @@
 sudo apt update
 sudo apt install -y nodejs npm git
-sudo npm i -g commitizen
-sudo npm i -D cz-customizable
+
+
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.profile
+source ~/.profile
+npm install -g jshint
+
+npm i -g commitizen
+npm i -g -D cz-customizable
+
+mv ~/.npm-global/lib/node_modules/cz-customizable ~/.npm-global/lib/node_modules/cz-customizable/cz-config-EXAMPLE.old.js
+cp commitizen.js ~/.npm-global/lib/node_modules/cz-customizable/cz-config-EXAMPLE.js
+
