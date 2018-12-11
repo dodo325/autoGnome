d9ff2871f1962a2a71e04c5a16be1e0ec8fe8eaa
dodo325 <dodo101000101@gmail.com>
2018-12-11 23:07:35 +0300

    fix(tools): krita allredy in ppa

--- install_tools.sh
+++ install_tools.sh
@@ -38,8 +38,8 @@ rm -f Release.key
 
 
 peints 'Install krita'
-sudo add-apt-repository ppa:kritalime/ppa
-sudo apt-get update
+#sudo add-apt-repository ppa:kritalime/ppa
+#sudo apt-get update
 sudo apt-get install -y krita krita-l10n
 
 
