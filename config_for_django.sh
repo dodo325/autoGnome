6ad8a4dd0e5a2266f399b0be8f38ee850c89cf7e
dodo325 <dodo101000101@gmail.com>
2019-02-09 02:33:56 +0300

    feat(components:     Компоненты): add todo-tree in VScode Plugins

--- config_for_django.sh
+++ config_for_django.sh
@@ -9,7 +9,7 @@ conda install -c anaconda anaconda-navigator
 chmod +x Anaconda.desktop 
 cp Anaconda.desktop ~/.local/share/applications
 
-# coede Plugins:
+# VScode Plugins:
 code --install-extension ms-ceintl.vscode-language-pack-ru
 code --install-extension donjayamanne.python-extension-pack
 code --install-extension thebarkman.vscode-djaneiro
@@ -24,3 +24,4 @@ code --install-extension zignd.html-css-class-completion
 code --install-extension rebornix.project-snippets
 code --install-extension knisterpeter.vscode-commitizen
 
+code --install-extension gruntfuggly.todo-tree
