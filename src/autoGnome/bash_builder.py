075e72dbd1e4a081449ddaf780e2abce2f4575d6
mar4elkin <googlcraftlol@gmail.com>
2020-01-12 17:38:20 +0300

    feat(src): collect_scripts

--- src/autoGnome/bash_builder.py
+++ src/autoGnome/bash_builder.py
@@ -7,6 +7,7 @@ import logging
 import argparse
 import os
 import json
+import sys
 logging.basicConfig(level=logging.INFO)
 
 def arg_parse():
@@ -51,15 +52,41 @@ def recursive_topological_sort(graph, node):
 def get_fils_list(path):
     pass
 
+def get_path(filename):
+    fpath = os.path.abspath(filename)
+    return fpath
+
+def collect_scripts():
+    scripts = get_scripts_list()
+    header = get_path("../Scripts/header.sh")
+    build_dir = get_path("../../build/install.sh")
+    
+    headerf = open(header, "r")
+    build_dirf = open(build_dir, "w")
+    build_dirf.write("")
+    build_dirf.close()
+
+    build_dirf = open(build_dir, "a") #a
+    build_dirf.write(headerf.read())
+    headerf.close()
+    
+    for script in scripts.keys():
+        files = get_path("../Scripts")+'/'+scripts[script]["name"]+'/'+scripts[script]['installers']['default']
+        filesf = open(files, "r")
+        build_dirf.write(filesf.read())
+    
+    build_dirf.close()
+
 def get_subdirs_list(path):
     return [dI for dI in os.listdir(path) if os.path.isdir(os.path.join(path,dI))]
 
 def get_scripts_list() -> dict:
-    subdirs = get_subdirs_list("../Scripts")
+    script_dir = get_path("../Scripts")
+    subdirs = get_subdirs_list(script_dir)
     sctipts = dict()
     for subdir in subdirs:
         try:
-            with open('../Scripts/'+subdir+'/info.json') as json_file:
+            with open(script_dir+'/'+subdir+'/info.json') as json_file:
                 sctipts[subdir] = json.load(json_file)
         except FileNotFoundError as e:
             logging.warning(f'[get_sctipts_list] {e}')
