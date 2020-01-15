5203192f30e24cc61faa06fcb4ab5652d8c87652
dodo325 <dodo101000101@gmail.com>
2020-01-15 03:15:32 +0300

    feat(bash_builder): CLI start work!

--- src/autoGnome/bash_builder.py
+++ src/autoGnome/bash_builder.py
@@ -15,10 +15,20 @@ def arg_parse():
     Parse arguments grom cli using argparse. 
     '''
 
-    logging.info(f'[arg_parse] init')
+    logging.debug(f'[arg_parse] init')
     parser = argparse.ArgumentParser(
         description=sys.modules[__name__].__doc__)
 
+    # scripts_dir
+    parser.add_argument("-s", "--scripts-dir", type=str, default=None,
+                        help="path to Scripts\ dir")
+    parser.add_argument("-d", "--build-dir", type=str, default=None,
+                        help="path to Scripts\ dir")
+                        # scripts_names
+    parser.add_argument('-n','--scripts-names', nargs='+', help='<Required> Set scripts_names', required=True)
+
+    args = vars(parser.parse_args())
+    logging.debug(f'[arg_parse] args= {args}') 
     return args
 
 def recursive_dfs(graph, node):
@@ -52,24 +62,28 @@ def recursive_topological_sort(graph, node):
 def get_fils_list(path):
     pass
 
-def get_path(filename):
-    fpath = os.path.dirname(os.path.abspath(filename + '/' + __file__)) #-> src_dir + Scrips
-    #fpath = os.path.abspath(filename)
-    return fpath
+def get_path_local(path: str):
+    return os.path.abspath(os.path.abspath(os.path.dirname(__file__))+"/"+path) #-> src_dir + Scrips
+     
 
-def get_subdirs_list(path):
+def get_subdirs_list(path: str):
     return [dI for dI in os.listdir(path) if os.path.isdir(os.path.join(path,dI))]
 
-def get_scripts_list() -> dict:
-    script_dir = get_path("Scripts")
-    subdirs = get_subdirs_list(script_dir)
+def get_scripts_list(scripts_dir: str = None) -> dict:
+    if not scripts_dir:
+        scripts_dir = get_path_local("../Scripts")
+    
+    logging.debug(f"[get_scripts_list] scripts_dir = {scripts_dir}")
+    
+    subdirs = get_subdirs_list(scripts_dir)
     sctipts = dict()
     for subdir in subdirs:
         try:
-            with open(script_dir+'/'+subdir+'/info.json') as json_file:
+            with open(scripts_dir+'/'+subdir+'/info.json') as json_file:
                 sctipts[subdir] = json.load(json_file)
         except FileNotFoundError as e:
             logging.warning(f'[get_sctipts_list] {e}')
+    logging.debug(f"[get_scripts_list] sctipts = {sctipts}")
     return sctipts
 
 def get_all_tags(scripts):
@@ -78,13 +92,13 @@ def get_all_tags(scripts):
         tags.update(data['tags'])
     return tags
 
-def get_scripts_dependences_order(scripts, scripts_name):
+def get_scripts_dependences_order(scripts: dict, scripts_names: list):
     graph = dict()
     for name, data in scripts.items():
         graph[name] = data['dependence']
     
     scripts_dependences = []
-    for name in scripts_name:
+    for name in scripts_names:
         script_dependences = recursive_topological_sort(graph, name)
         for scripts_dependence in script_dependences:
             if scripts_dependence not in scripts_dependences:
@@ -92,13 +106,13 @@ def get_scripts_dependences_order(scripts, scripts_name):
     return scripts_dependences
 
 
-def collect_scripts(names: list, scripts: dict, build_dir: str = None) -> None:
-    logging.debug(f"[collect_scripts] names = {names}")
-    scripts_dependences = get_scripts_dependences_order(names, scripts)
-    header = get_path("../Scripts/header.sh")
+def collect_scripts(scripts_names: list, scripts: dict, build_dir: str = None) -> None:
+    logging.debug(f"[collect_scripts] names = {scripts_names}")
+    scripts_dependences = get_scripts_dependences_order(scripts, scripts_names)
+    header = get_path_local("../Scripts/header.sh")
 
     if not build_dir:
-        build_dir = get_path("../../build/install.sh")
+        build_dir = get_path_local("../../build/install.sh")
     
     headerf = open(header, "r")
     build_dirf = open(build_dir, "w")
@@ -111,8 +125,8 @@ def collect_scripts(names: list, scripts: dict, build_dir: str = None) -> None:
     headerf.close()
     
     for script in scripts_dependences:
-        logging.debug(f"[collect_scripts] script = {script}")
-        install_file = get_path("../Scripts")+'/'+script+'/'+scripts[script]['installers']['default']
+        logging.info(f"[collect_scripts] script = {script}")
+        install_file = get_path_local("../Scripts")+'/'+script+'/'+scripts[script]['installers']['default']
         install_filef = open(install_file, "r")
         logging.debug(f"[collect_scripts] install_file = {install_file}")
         build_dirf.write("\n")
@@ -123,7 +137,12 @@ def collect_scripts(names: list, scripts: dict, build_dir: str = None) -> None:
 def main():
     global args
     args = arg_parse()
-
+    scripts = get_scripts_list(scripts_dir=args['scripts_dir'])
+    collect_scripts(
+        scripts_names = args['scripts_names'], 
+        scripts = scripts, 
+        build_dir = args['build_dir'],
+        )
 if __name__ == "__main__":
   main()
 
