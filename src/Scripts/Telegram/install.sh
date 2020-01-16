58b607813343fa0b26af51afcfcfa87f6b8f10de
dodo325 <dodo101000101@gmail.com>
2020-01-16 19:03:31 +0300

    fix(Scripts/Telegram): -y

--- src/Scripts/Telegram/install.sh
+++ src/Scripts/Telegram/install.sh
@@ -1,3 +1,4 @@
-sudo add-apt-repository ppa:atareao/telegram
+peints 'Install Telegram'
+sudo add-apt-repository ppa:atareao/telegram -y
 sudo apt-get update
-sudo apt-get install telegram
+sudo apt-get install telegram -y
