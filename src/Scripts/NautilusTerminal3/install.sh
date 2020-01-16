383075ad0f831be6369842c184e28c2e7896bc16
dodo325 <dodo101000101@gmail.com>
2020-01-16 19:21:19 +0300

    fix(Scripts/NautilusTerminal3): another installer

--- src/Scripts/NautilusTerminal3/install.sh
+++ src/Scripts/NautilusTerminal3/install.sh
@@ -1,5 +1,9 @@
 peints 'Install Nautilus Terminal 3' #https://www.linuxuprising.com/2018/09/get-terminal-embedded-in-nautilus-file.html
-sudo apt install python-pip python-nautilus python-psutil
-pip2 install --user nautilus_terminal
+# sudo apt install python-pip python-nautilus python-psutil
+# pip2 install --user nautilus_terminal
+sudo add-apt-repository ppa:flozz/nautilus-terminal -y
+sudo apt update
+sudo apt install nautilus-terminal -y
+
 nautilus -q
 sudo apt install dconf-editor -y
