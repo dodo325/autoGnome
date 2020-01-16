bd26f2a3a4e0ea5b5ec443361b39b3ddd8a3effa
dodo325 <dodo101000101@gmail.com>
2020-01-16 19:01:34 +0300

    fix(Scripts/Synergy): y

--- src/Scripts/Synergy/install.sh
+++ src/Scripts/Synergy/install.sh
@@ -1,3 +1,3 @@
 peints 'Install Synergy'
-sudo apt-get install libcanberra-gtk-module sni-qt
-sudo apt install synergy
+sudo apt-get install libcanberra-gtk-module sni-qt -y
+sudo apt install synergy -y
