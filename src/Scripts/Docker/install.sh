95f5189f0b2f7a35e421a8360546809a85ec00e3
dodo325 <dodo101000101@gmail.com>
2020-03-02 18:04:52 +0300

    fix(Scripts): docker

--- src/Scripts/Docker/install.sh
+++ src/Scripts/Docker/install.sh
@@ -1,11 +1,17 @@
 peints 'Install Docker'
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-rm get-docker.sh
+curl -fsSL https://get.docker.com -o get-docker.sh; sh get-docker.sh; rm get-docker.sh
 
 peints 'Install Docker: add in systemctl'
+sudo systemctl enable docker
 sudo systemctl status --no-pager docker
 
 peints 'Install Docker: add user in docker groupe'
 sudo usermod -aG docker ${USER}
 
+peints 'Install Docker-compose: add user in docker groupe'
+
+sudo apt-get install libffi-dev libssl-dev -y
+
+sudo apt-get install -y python python-pip
+
+sudo apt-get remove python-configparser -y
